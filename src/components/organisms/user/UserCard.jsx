c36cbec15f730c393fcd6099d798c673b6f5e15c
shu-suzuki-1124 <103195099+shu-suzuki-1124@users.noreply.github.com>
2022-04-29 20:23:41 +0900

    atomic design

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>aaaaaa@example.com</dd>
+        <dt>TEL</dt>
+        <dd>123-456-789</dd>
+        <dt>会社名</dt>
+        <dd>aaaacompany</dd>
+        <dt>web</dt>
+        <dd>http//会社</dd>
+      </dl>
+    </div>
+  );
+};
