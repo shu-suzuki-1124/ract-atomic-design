cb71830465265eb026b9c73c5fbfd38a861a60a0
shu-suzuki-1124 <103195099+shu-suzuki-1124@users.noreply.github.com>
2022-04-30 22:49:17 +0900

    グローバルステート

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
+import { useLocation } from "react-router-dom";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
