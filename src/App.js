ba7ab0b52170e7452189a1a4fddfb250174be5b4
shu-suzuki-1124 <103195099+shu-suzuki-1124@users.noreply.github.com>
2022-04-30 19:16:59 +0900

    atomic-design page-templetes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
+import { Router } from "./routes/Router";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <br />
-      <UserCard />
-    </div>
-  );
+  return <Router />;
 }
